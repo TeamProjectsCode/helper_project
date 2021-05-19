311bd3b78e85d41b244afd8177f508855db141d9
ju <gbl1001@naver.com>
2021-05-19 19:06:24 +0900

    0519
    reviewPost

--- src/db/ReviewPostDAO.java
+++ src/db/ReviewPostDAO.java
@@ -1,115 +1,43 @@
 package db;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.text.SimpleDateFormat;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
-import db.jobBoardBeans.JobPostBean;
 import db.reviewBeans.ReviewPostBean;
-import db.userBeans.UserBean;
-import myUtil.HanConv;
 
 public class ReviewPostDAO {
-//	INSERT INTO REVIEW_BOARD(NO
-//            , CREATED_AT
-//            , CREATOR
-//            , REVIEW_TITLE
-//            , REVIEW_HITS
-//            , REVIEW_SCORE
-//            , REVIEW_DETAIL
-//            , JOB_POST
-//            ) 
-//VALUES(1
-//            , CURRENT_TIMESTAMP
-//            , 1
-//            , '府轰 力格'
-//            , 0
-//            , 5
-//            , '府轰 叼抛老'
-//            , 1
-//             );	
-	
-
-	
-	private static ReviewPostDAO instance=new ReviewPostDAO();
+	private static ReviewPostDAO instance = new ReviewPostDAO();
 	
 	public static ReviewPostDAO getInstance() {
 		return instance;
 	}
-	
-//	public Connection getConnection() throws Exception{
-//		Context ctx=new InitialContext();
-//		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
-//		return ds.getConnection();
-//	}
-	public int insertReview(String title, String detail){
-		UserBean ub = new UserBean();
-		JobPostBean jpb = new JobPostBean();
-//		ReviewPostBean rpb = new ReviewPostBean();
-//		SimpleDateFormat sdf=new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
-//		String ss=sdf.format(new java.util.Date());
-//		Date createdAt= Date.valueOf(ss);
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		String sql="";
-		int re = -1;
-
-		
+	public int insertReview(ReviewPostBean rb) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = "";
+		int re=-1;
 		try {
 			con = DBConnection.getConnection();
-			System.out.println(title);
-			System.out.println(detail);
-
+			
 			sql="INSERT INTO REVIEW_BOARD(NO, CREATOR_NO, TITLE, DETAIL, JOB_POST_NO) VALUES(REVIEW_BOARD_NO_SEQ.nextval, ?, ?, ?, ?)";
-			
+			int creator = (rb.getCreator());
+			String title = (rb.getReview_titile());
+			String review = (rb.getReview_detail());
 			pstmt=con.prepareStatement(sql);
-			pstmt.setInt(1, 2);
+			pstmt.setInt(1, creator);
 			pstmt.setString(2, title);
-			pstmt.setString(3, detail);
+			pstmt.setString(3, review);
 			pstmt.setInt(4, 4);
-			
 			pstmt.executeUpdate();
 			re=1;
 			pstmt.close();
 			con.close();
-			System.out.println("府轰累己 己傍");
-		} catch (Exception e) {
-			System.out.println("府轰累己 角菩");
+			System.out.println("毽钒鞛戩劚 靹标车");
+			} catch (Exception e) {
+			System.out.println("毽钒鞛戩劚 鞁ろ尐");
 			e.printStackTrace();
 			re=-1;
-		} 
+		}
 		return re;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
